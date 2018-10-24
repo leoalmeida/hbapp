b1ae545780c584cb05fd0949e9cbf62167821232
Leonardo Almeida <leoalmeida.rj@gmail.com>
2018-10-23 21:14:49 -0300

    Correção na leitura da informação

--- src/app/heartbeat/heartbeat.page.ts
+++ src/app/heartbeat/heartbeat.page.ts
@@ -6,104 +6,105 @@ import {IClientOptions, MqttClient} from 'mqtt';
 
 export interface MqttMessage {
 	from: string;
-    body: MessageBody;
+	notification: MessageBody;
 }
 
 export interface MessageBody {
 	title: string;
-    //type: string;
-    data: string;
+	//type: string;
+	data: string;
 }
 
 @Component({
-  selector: 'app-heartbeat',
-  templateUrl: './heartbeat.page.html',
-  styleUrls: ['./heartbeat.page.scss'],
+	selector: 'app-heartbeat',
+	templateUrl: './heartbeat.page.html',
+	styleUrls: ['./heartbeat.page.scss'],
 })
 export class HeartbeatPage implements  AfterViewInit, OnDestroy  {
 	@ViewChild("heartbeatcanvas") myCanvas;
-	rectW:number = 100;
-	rectH:number =  100;
+	rectW:number = 50;
+	rectH:number =  50;
 	rectColor:string = "#FF0000";
 	context:CanvasRenderingContext2D;
-	
+
 	private counter = 0;
 
 	messages: Array<MqttMessage> = [];
 
-    status: Array<string> = [];
-	
+	status: Array<string> = [];
+
 	ball: any = {};
 	point: any = {};
 	current_point: number = 0;
-	
+
 	points:any = [];
 
-    constructor(private _mqttService: MqttService) {
+	constructor(private _mqttService: MqttService) {
 		this._mqttService.status().subscribe((s: ConnectionStatus) => {
-            const status = s === ConnectionStatus.CONNECTED ? 'CONNECTED' : 'DISCONNECTED';
-            this.status.push(`Mqtt client connection status: ${status}`);
-        });
+			const status = s === ConnectionStatus.CONNECTED ? 'CONNECTED' : 'DISCONNECTED';
+			this.status.push(`Mqtt client connection status: ${status}`);
+		});
 		this.subscribe();
 		this.sendMsg();
-		this.points.push({y:0,x:10})
-    }
-	
+		this.points.push({y:0,x:10});
+	}
+
 	connect(config: IClientOptions): void {
-        this._mqttService.connect(config);
-    }
-	
-	 subscribe(): void {
-        this._mqttService.subscribeTo<MqttMessage>('esp/000001/RAW')
-            .subscribe({
-                next: (msg: SubscriptionGrant | MqttMessage) => {
-                    if (msg instanceof SubscriptionGrant) {
-                        this.status.push('Subscribed to esp/000001/RAW topic!');
-                    } else {
+		this._mqttService.connect(config);
+	}
 
-                        this.messages.push(msg);
-						this.points.push({y:Number(msg.body.data),x:10})
-                    }
-                },
-                error: (error: Error) => {
-                    this.status.push(`Something went wrong: ${error.message}`);
-                }
-            });
-    }
+	subscribe(): void {
+		this._mqttService.subscribeTo<MqttMessage>('esp/000001/RAW')
+		.subscribe({
+			next: (msg: SubscriptionGrant | MqttMessage) => {
+				if (msg instanceof SubscriptionGrant) {
+					this.status.push('Subscribed to esp/000001/RAW topic!');
+				} else {
+
+					this.messages.push(msg);
+					//this.points.push({y:Number(msg.notification.data),x:10});
+					this.points[0] = {y:Number(msg.notification.data),x:10};
+				}
+			},
+			error: (error: Error) => {
+				this.status.push(`Something went wrong: ${error.message}`);
+			}
+		});
+	}
 
 
-    sendMsg(): void {
-        this._mqttService.publishTo<MessageBody>('esp/000001/info', {title: '', data: 'teste'}).subscribe({
-            next: () => {
-                this.status.push('Message sent to esp/000001/info topic');
-            },
-            error: (error: Error) => {
-                this.status.push(`Something went wrong: ${error.message}`);
-            }
-        });
-    }
+	sendMsg(): void {
+		this._mqttService.publishTo<MessageBody>('esp/000001/info', {title: '', data: 'teste'}).subscribe({
+			next: () => {
+				this.status.push('Message sent to esp/000001/info topic');
+			},
+			error: (error: Error) => {
+				this.status.push(`Something went wrong: ${error.message}`);
+			}
+		});
+	}
 
-    /**
-     * Unsubscribe from fooBar topic.
-     */
-    unsubscribe(): void {
-        this._mqttService.unsubscribeFrom('esp/000001/RAW').subscribe({
-            next: () => {
-                this.status.push('Unsubscribe from esp/000001/RAW topic');
-            },
-            error: (error: Error) => {
-                this.status.push(`Something went wrong: ${error.message}`);
-            }
-        });
-    }
+	/**
+	* Unsubscribe from fooBar topic.
+	*/
+	unsubscribe(): void {
+		this._mqttService.unsubscribeFrom('esp/000001/RAW').subscribe({
+			next: () => {
+				this.status.push('Unsubscribe from esp/000001/RAW topic');
+			},
+			error: (error: Error) => {
+				this.status.push(`Something went wrong: ${error.message}`);
+			}
+		});
+	}
 
-    /**
-     * The purpose of this is, when the user leave the app we should cleanup our subscriptions
-     * and close the connection with the broker
-     */
-    ngOnDestroy(): void {
-        this._mqttService.end();
-    }
+	/**
+	* The purpose of this is, when the user leave the app we should cleanup our subscriptions
+	* and close the connection with the broker
+	*/
+	ngOnDestroy(): void {
+		this._mqttService.end();
+	}
 
 	ngAfterViewInit() {
 		let canvas = this.myCanvas.nativeElement;
@@ -123,11 +124,11 @@ export class HeartbeatPage implements  AfterViewInit, OnDestroy  {
 
 		this.context.fillStyle = "rgba(255, 0, 0, 1)";
 		this.render();
-	  }
+	}
 
-	  render(){
+	render(){
 		requestAnimationFrame(()=> {
-		  this.render()
+			this.render()
 		});
 		this.animateTo();
 		var ctx = this.context;
@@ -138,36 +139,37 @@ export class HeartbeatPage implements  AfterViewInit, OnDestroy  {
 		ctx.arc(this.ball.x, this.ball.y, 1, 0, 2*Math.PI, true);
 		ctx.closePath();
 		ctx.fill();
-	  }
+	}
 
-	  animateTo() {
+	animateTo() {
+		this.current_point = 0;
 		var dis = this.dist(this.ball.x, this.point.x+this.points[this.current_point].x,this.ball.y, this.point.y+this.points[this.current_point].y);
 		if( dis.d > 1 ) {
-		  var s = Math.abs(dis.dy) > 13 ? 2 : 1;
-		  this.ball.x += -( dis.dx / dis.d )*s;
-		  this.ball.y += -( dis.dy / dis.d )*s;
+			var s = Math.abs(dis.dy) > 13 ? 2 : 1;
+			this.ball.x += -( dis.dx / dis.d )*s;
+			this.ball.y += -( dis.dy / dis.d )*s;
 		} else {
-		  this.ball.x = this.point.x+this.points[this.current_point].x;
-		  this.ball.y = this.point.y+this.points[this.current_point].y;
-		  this.point.x += this.points[this.current_point].x;
-		  this.current_point++;
-		  if( this.current_point >= this.points.length || this.ball.x > this.rectW ) {
-			this.current_point = 0;
-			if( this.ball.x > this.rectW ) {
-			  this.point.x = this.ball.x = 0;
+			this.ball.x = this.point.x+this.points[this.current_point].x;
+			this.ball.y = this.point.y+this.points[this.current_point].y;
+			this.point.x += this.points[this.current_point].x;
+			this.current_point++;
+			if( this.current_point >= this.points.length || this.ball.x > this.rectW ) {
+				this.current_point = 0;
+				if( this.ball.x > this.rectW ) {
+					this.point.x = this.ball.x = 0;
+				}
 			}
-		  }
 		}
-	  }
+	}
 
-	  dist(x1,x2,y1,y2) {
+	dist(x1,x2,y1,y2) {
 		var dx = x1 - x2,
-		  dy = y1 - y2;
+		dy = y1 - y2;
 		return {
-		  d: Math.sqrt(dx*dx + dy*dy),
-		  dx: dx,
-		  dy: dy
+			d: Math.sqrt(dx*dx + dy*dy),
+			dx: dx,
+			dy: dy
 		};
-	  }
+	}
 
 }
